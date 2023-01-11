072731700a810b8ed0b3f419d7d9a4b3012bcd93
Chivu Marius <chivumarius@yahoo.com>
2023-01-11 06:16:13 +0200

    OTP Screen UI

--- lib/features/auth/screens/otp_screen.dart
+++ lib/features/auth/screens/otp_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_flutter/colors.dart';
 
 class OTPScreen extends StatefulWidget {
 
@@ -20,6 +21,52 @@ class _OTPScreenState extends State<OTPScreen> {
   // ♦ The "build()" Method:
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    // ♦ "Media Query":
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      // ♦ "APP BAR":
+      appBar: AppBar(
+        title: const Text('Verifying your number'),
+        elevation: 0,
+        backgroundColor: backgroundColor,
+      ),
+
+      // ♦ "BODY":
+      body: Center(
+        child: Column(
+          children: [
+            // ♦ Vertical Spacing
+            const SizedBox(height: 20),
+
+            // ♦ Title:
+            const Text('We have sent an SMS with a code.'),
+
+            // ♦ "Hint Text":
+            SizedBox(
+              // ♦ Using "Media Query":
+              width: size.width * 0.5,
+
+              // ♦ "Input Text Field"::
+              child: TextField(
+                textAlign: TextAlign.center,
+                decoration: const InputDecoration(
+                  hintText: '- - - - - -',
+                  hintStyle: TextStyle(
+                    fontSize: 30,
+                  ),
+                ),
+
+                // ♦ "Number Keyboard":
+                keyboardType: TextInputType.number,
+
+                // ♦ Functionality on Changed:
+                onChanged: (val) {},
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
